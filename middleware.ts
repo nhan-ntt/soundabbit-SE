718d20263c19edd91a1a7d98880b5ab73ef57aea
png261 <nhphuong.code@gmail.com>
2024-04-18 10:30:38 +0700

    feat: prevent to '/login', '/register', '/' page after authenticated

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,34 @@
-export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export default async function middleware(req: NextRequest, event: NextFetchEvent) {
+    const token = await getToken({ req });
+    const isAuthenticated = !!token;
+
+    if (["/login", "/register", "/"].includes(req.nextUrl.pathname)) {
+        if (isAuthenticated) {
+            return NextResponse.redirect(new URL('/home', req.url));
+        }
+
+        return NextResponse.next();
+    }
+
+    const authMiddleware = await withAuth({
+        pages: {
+            signIn: `/login`,
+        },
+    });
+
+    // @ts-expect-error
+    return authMiddleware(req, event);
+}
 
 export const config = {
     matcher: [
-        "/artist/:path*",
-        "/genre/:path*",
-        "/playlist/:path*",
-        "/account",
+        "/",
+        "/login",
+        "/register",
         "/home",
         "/index",
         "/library",
@@ -13,6 +36,10 @@ export const config = {
         "/providers",
         "/queue",
         "/search",
+        "/artist/:path*",
+        "/genre/:path*",
+        "/playlist/:path*",
+        "/account",
     ]
 }
 
